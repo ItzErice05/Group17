45efbe5379ff01341d2099c71058627748c6ad75
Erice <ecama@gbox.adnu.edu.ph>
2025-10-06 21:37:17 +0800

    I added these two for now

---
+++ template.js
@@ -0,0 +1,6 @@
+//the stuff we gotta import ig
+//npm install express mongoose dotenv cors
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
