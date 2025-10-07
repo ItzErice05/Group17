aae2cc2c5b875869ba47bddcf36f3143f33de0fe
Giankarlo Rivera <signmein3456@gmail.com>
2025-10-07 12:32:30 +0800

    minor bug fixes

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     id: { type: String,  unique:true, default: () => uuidv4() },
-    username: { type: String, required: true, trime: true },
+    username: { type: String, required: true, trim: true },
     password: { type: String, required: true },
     role: { type: String, enum: ['admin', 'user'], default: 'user' },
 })
