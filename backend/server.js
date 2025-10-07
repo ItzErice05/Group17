98dfc5f9c18d7c167a0dda3f7556432368836f2b
hazzara <hjazur@gbox.adnu.edu.ph>
2025-10-07 12:53:12 +0800

    Add api functionality for accounts and transactions

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const userRoutes = require('./routes/userRoutes');
+const accountRoute = require('./routes/accountRoute');
+const transactionsRoute = require('./routes/transactionsRoute');
 
 const app = express();
 
@@ -11,6 +13,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api', userRoutes);
+app.use('/api/accounts', accountRoute);        
+app.use('/api/transactions', transactionsRoute);
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
