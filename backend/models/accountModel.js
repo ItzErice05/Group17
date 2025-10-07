f0bece5a91bac0ff00723fa87cae2d972794a1c2
hazzara <hjazur@gbox.adnu.edu.ph>
2025-10-07 10:11:01 +0800

    Change balance type

--- backend/models/accountModel.js
+++ backend/models/accountModel.js
@@ -1,12 +1,11 @@
 const { v4: uuidv4 } = require('uuid');
 const mongoose = require('mongoose');
-const { float } = require('webidl-conversions');
 
 const accountSchema = new mongoose.Schema({
     id: { type: String,  unique:true, default: () => uuidv4() },
     userId : { type: String, required: true, trim: true },
     accountType: { type: String, required: true },
-    balance: { type: float, default: 0.0 },
+    balance: { type: Number, default: 0.0 },
 })
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', accountSchema);
