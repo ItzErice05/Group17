195b63d0ae5c9a3c66a4420da408efd802bea369
Giankarlo Rivera <signmein3456@gmail.com>
2025-10-07 12:02:04 +0800

    implemented register user

---
+++ backend/services/userService.js
@@ -0,0 +1,20 @@
+const bcrypt = require('bcrypt');
+const user = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+
+const registerUser = async (userData) => {
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(userData.password, saltRounds);
+    
+    const newUser = new user({ 
+        ...userData, 
+        password: hashedPassword 
+    });
+    
+    const savedUser = await newUser.save();
+
+    const userObject = savedUser.toObject();
+    delete userObject.password;
+
+    return userObject;
+}
